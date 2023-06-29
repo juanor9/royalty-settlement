27c0368d19cdf7c5e763bcd5a989440ae23f064e
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-06-29 08:04:42 -0500

    Set code to include data only for requested product code

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ import * as readline from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
 import * as fs from "fs";
 import * as xml2js from "xml2js";
+import { type } from "node:os";
 
-// const rl = readline.createInterface({ input, output });
+const rl = readline.createInterface({ input, output });
 
 // const author = await rl.question(
 //   "¿A qué autor quieres hacer la liquidación de regalías? "
 // );
-// const bookListString = await rl.question(
-//   "Por favor indica los isbn de los libros sobre los que quieres liquidar regalías. Incluye el código tal cual está registrado en las facturas. Separa cada código por una coma (,). "
-// );
-// const bookList = bookListString.split(",").map((book) => book.trim());
+const bookListString = await rl.question(
+  "Por favor indica los isbn de los libros sobre los que quieres liquidar regalías. Incluye el código tal cual está registrado en las facturas. Separa cada código por una coma (,). "
+);
+const bookList = bookListString.split(",").map((book) => book.trim());
 
 // const percentages = {};
 
@@ -20,74 +21,73 @@ import * as xml2js from "xml2js";
 //   percentages[book] = percentage;
 // };
 
-// rl.close();
+rl.close();
 
 // Analizar xml
 const path = "./xml-billing";
 
-const files = await fs.promises.readdir(path);
-// console.log("🚀 ~ file: index.js:31 ~ files:", files)
-files.forEach(async (file) => {
-  const extension = file.split(".")[1];
-  if (extension !== "xml") {
-    return; // Ignorar archivos que no sean XML
-  }
+const files = await fs.promises.readdir(path); //Lista de todos los archivos en path
+const allFilesPath = files.map((file) => {
   const filePath = `${path}/${file}`;
-
-  // console.log("🚀 ~ file: index.js:39 ~ filePath:", filePath)
-
-  const content = await fs.promises.readFile(filePath, "utf-8");
-  const result = await new Promise((resolve, reject) => {
-    xml2js.parseString(content, (err, result) => {
-      // Analizar el contenido XML
-      if (err) {
-        reject(err);
-      } else {
-        resolve(result);
-      }
-    });
-  });
-
-  const FeDataDescription =
-    result.AttachedDocument["cac:Attachment"][0]["cac:ExternalReference"][0][
-      "cbc:Description"
-    ][0];
-
-  const FeDataDescriptionResult = await new Promise((resolve, reject) => {
-    const parser = new xml2js.Parser();
-    parser.parseString(FeDataDescription, (err, result) => {
-      // Analizar la descripción de los datos adjuntos
-      if (err) {
-        reject(err);
-      } else {
-        resolve(result);
-      }
-    });
-  });
-  const FeId = result.AttachedDocument["cbc:ParentDocumentID"][0];
-  const itemXml = FeDataDescriptionResult.Invoice["cac:InvoiceLine"];
-
-  const itemsArr = itemXml.map((item) => {
-    const ammount = Number(item["cbc:InvoicedQuantity"][0]["_"]);
-    const bookName = item["cac:Item"][0]["cbc:Description"][0];
-    const code =
-      item["cac:Item"][0]["cac:StandardItemIdentification"][0]["cbc:ID"][0][
-        "_"
-      ];
-    const unitPrice = Number(item["cac:Price"][0]["cbc:PriceAmount"][0]["_"]);
-
-    const itemData = {
-      FeId: FeId,
-      ammount: ammount,
-      name: bookName,
-      code: code,
-      unitPrice: unitPrice,
-      totalPrice: unitPrice * ammount,
-    };
-    return itemData;
-  });
-  console.log("🚀 ~ file: index.js:89 ~ itemsArr ~ itemsArr:", itemsArr)
+  const extension = file.split(".")[1];
+  if (extension === "xml") {
+    return filePath;
+  } else {
+    return null;
+  }
 });
+const xmlFilesPath = allFilesPath.filter((file) => file !== null); // Lista de ruta de todos los archivos xml
+
+const xmlFilesData = await Promise.all(
+  xmlFilesPath.map(async (file) => {
+    const xmlBuffer = await fs.promises.readFile(file);
+    const xmlString = xmlBuffer.toString("utf-8");
+
+    const fullXmlContent = await new Promise((resolve, reject) => {
+      // Devuelve un objeto con toda la informacion del archivo xml
+      xml2js.parseString(xmlString, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+
+    const dataDescriptionString =
+      fullXmlContent.AttachedDocument["cac:Attachment"][0][
+        "cac:ExternalReference"
+      ][0]["cbc:Description"][0]; // Devuelve un string con el xml de la descripcion de la factura
+    const dataDescription = await new Promise((resolve, reject) => {
+      // Devuelve un objeto con toda la informacion de la descripcion archivo xml
+      xml2js.parseString(dataDescriptionString, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+    const itemsXml = dataDescription.Invoice["cac:InvoiceLine"];
+    const itemsArray = itemsXml.map((item) => {
+      const code =
+        item["cac:Item"][0]["cac:StandardItemIdentification"][0]["cbc:ID"][0][
+          "_"
+        ];
+
+      for (const book of bookList) {
+        if (code === book) {
+          return code;
+        } else {
+          return;
+        }
+      }
+    });
+
+    return itemsArray;
+  })
+);
+console.log("🚀 ~ file: index.js:46 ~ xmlFilesData:", xmlFilesData);
 
 // console.log(`Autor: ${author}`);
 // console.log('Libros:')
